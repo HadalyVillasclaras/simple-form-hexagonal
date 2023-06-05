7142268b6528aaa98590d69836716f8104023201
Hadaly <hadalyvf@gmail.com>
2023-06-06 00:11:52 +0200

    feat svg circle

--- front/ui/components/MagneticCircle/magneticCircle.ts
+++ front/ui/components/MagneticCircle/magneticCircle.ts
@@ -1,6 +1,7 @@
 export function magneticCircle() {
   const circle = document.getElementById('circle') as HTMLElement;
   const circleText = document.getElementById('circle-text') as HTMLElement;
+  const svgCircle = document.getElementById('svg-circle') as HTMLElement;
 
   circle.addEventListener('mousemove', (event) => {
     const rect = circle.getBoundingClientRect();
@@ -8,7 +9,7 @@ export function magneticCircle() {
     const y = event.clientY - rect.top;
     
     // elements ratio 
-    const ratioCircle = 0.8;  
+    const ratioCircle = 1.5;  
     const ratioText = 0.1; 
 
     const moveXCircle = (x - rect.width / 2) * ratioCircle;
@@ -18,12 +19,15 @@ export function magneticCircle() {
     const moveYText = (y - rect.height / 2) * ratioText;
 
     // transform
-    circle.style.transform = `scale(1.5) translate3d(${moveXCircle}px, ${moveYCircle}px, 0)`;
+    circle.style.transform = `scale(1.4) translate3d(${moveXCircle}px, ${moveYCircle}px, 0)`;
     circleText.style.transform = `translate3d(${moveXText}px, ${moveYText}px, 0)`;
+    svgCircle.classList.add('rotateCircleText');
   });
 
   circle.addEventListener('mouseleave', () => {
     circle.style.transform = `translate3d(0, 0, 0)`;
     circleText.style.transform = `translate3d(0, 0, 0)`;
+    svgCircle.classList.remove('rotateCircleText');
   });
+
 }
